0f3cb34952a1f28765c8883b4749789d36497c6c
grzesiu <grzspychala@gmail.com>
2017-04-30 12:25:59 +0200

    Calculate possible actions

--- utils.py
+++ utils.py
@@ -1,7 +1,17 @@
 import math
 
-def get_poisson(lam, n):
+
+def poisson(lam, n):
     return lam ** n * math.exp(-lam) / math.factorial(n)
 
+
+def poisson_probs(lam, max_n):
+    probs = [poisson(lam, n) for n in range(max_n)]
+    probs.append(1 - sum(probs))
+    return probs
+
+
 if __name__ == "__main__":
-    print(get_poisson(10, 2))
+    probs = poisson_probs(4, 20)
+    print(probs)
+    print(len(probs))
