f1ed66d6c2bab5f501c0085b40a37bff365a691d
grzesiu <grzspychala@gmail.com>
2017-05-07 17:53:27 +0200

    Calculate reward properly

--- cars_solution.py
+++ cars_solution.py
@@ -2,22 +2,20 @@ import numpy as np
 import poisson
 
 class Simulation:
-
     REWARD = 100
     PENALTY = 20
     RENTAL1 = (3, 3)
     RENTAL2 = (4, 2)
-    MAX_CARS = 10
-    MAX_MOVED = 2
+    MAX_CARS = 20
+    MAX_MOVED = 5
     DISCOUNT_FACTOR = 0.9
 
-
     def __init__(self):
+        self.shape = (self.MAX_CARS + 1, self.MAX_CARS + 1)
         self.rental1 = self.rental_probs(self.RENTAL1)
         self.rental2 = self.rental_probs(self.RENTAL2)
-
-        self.shape = (self.MAX_CARS + 1, self.MAX_CARS + 1)
-
+        self.prizes1 = self.prizes(self.RENTAL1)
+        self.prizes2 = self.prizes(self.RENTAL2)
         self.utils = np.zeros(self.shape)
         self.init_policy()
 
@@ -26,6 +24,15 @@ class Simulation:
         returns = poisson.TopDown.left_shift(lambdas[1], self.MAX_CARS)
         return rents.matrix @ returns.matrix
 
+    def prizes(self, lambdas):
+        rents = poisson.BottomUp.right_shift(lambdas[0], self.MAX_CARS)
+        returns = poisson.TopDown.left_shift(lambdas[1], self.MAX_CARS)
+        prizes = np.zeros(self.shape)
+        for (y, x) in np.ndindex(self.shape):
+            if x > y:
+                prizes[y][x] = Simulation.REWARD * (x - y)
+        return (rents.matrix * prizes) @ returns.matrix
+
     def init_policy(self):
         self.policy = np.empty(self.shape, dtype=np.int)
         for (y, x) in np.ndindex(self.shape):
@@ -33,7 +40,7 @@ class Simulation:
 
     def run(self, k):
         z = 0
-        while(True):
+        while True:
             for i in range(k):
                 self.step()
             next_policy = self.next_policy()
@@ -54,33 +61,35 @@ class Simulation:
         for (y, x) in np.ndindex(self.shape):
             for (yn, xn) in np.ndindex(self.utils.shape):
                 next_utils += self.summand(self.policy[y][x], y, x, yn, xn)
-        self.utils = next_utils
+        self.utils = next_utils / sum(next_utils)
 
     def summand(self, action, y, x, yn, xn):
-        return self.rental1[y - action][yn] * self.rental2[x + action][xn] * \
-            (self.PENALTY * abs(action) + self.DISCOUNT_FACTOR * self.utils[yn][xn])
+        ya = y - action
+        xa = x + action
+        reward = self.PENALTY * abs(action) + self.prizes1[ya][yn] * self.prizes2[xa][xn]
+        return self.rental1[ya][yn] * self.rental2[xa][xn] * (reward + self.DISCOUNT_FACTOR * self.utils[yn][xn])
 
     def next_policy(self):
-        policy = np.empty(self.shape, dtype=np.int)
+        next_policy = np.empty(self.shape, dtype=np.int)
         for (y, x) in np.ndindex(self.shape):
             f = lambda a: sum([self.summand_policy(a, y, x, yn, xn) for (yn, xn) in np.ndindex(self.utils.shape)])
-            policy[y][x] = max(self.possible_actions(y, x), key=f)
-        return policy
+            next_policy[y][x] = max(self.possible_actions(y, x), key=f)
+        return next_policy
 
     def summand_policy(self, action, y, x, yn, xn):
         return self.rental1[y - action][yn] * self.rental2[x + action][xn] * self.utils[yn][xn]
 
     def possible_actions(self, y, x):
-        return np.arange(-min(x, self.MAX_CARS - y), min(y, self.MAX_CARS - x) + 1, dtype=np.int)
+        return np.arange(-min(x, Simulation.MAX_CARS - y, Simulation.MAX_MOVED),
+                         min(y, Simulation.MAX_CARS - x, Simulation.MAX_MOVED) + 1, dtype=np.int)
+
 
 if __name__ == "__main__":
     np.set_printoptions(formatter={'float': '{: 0.2f}'.format}, linewidth=200)
     s = Simulation()
-    s.run(5)
+    s.run(1)
 
-#cars_solution.py:56: RuntimeWarning: overflow encountered in add
+# cars_solution.py:56: RuntimeWarning: overflow encountered in add
 #  next_utils += self.summand(self.policy[y][x], y, x, yn, xn)
-#cars_solution.py:61: RuntimeWarning: invalid value encountered in double_scalars
+# cars_solution.py:61: RuntimeWarning: invalid value encountered in double_scalars
 #  (self.PENALTY * abs(action) + self.DISCOUNT_FACTOR * self.utils[yn][xn])
-
-
