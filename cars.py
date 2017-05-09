5d1e77ffcc45399b63d9e591f937c10a0adb74e9
grzesiu <grzspychala@gmail.com>
2017-05-09 09:01:36 +0200

    Calculate rewards

--- cars.py
+++ cars.py
@@ -3,8 +3,12 @@ import math
 
 CAR_COST = 100
 MOVE_COST = 20
-RENTAL1 = (3, 3)
-RENTAL2 = (4, 2)
+
+RENTS_1 = 3
+RENTS_2 = 4
+RETURNS_1 = 3
+RETURNS_2 = 2
+
 MAX_CARS = 20
 MAX_MOVED = 5
 DISCOUNT_FACTOR = 0.9
@@ -18,11 +22,36 @@ def poisson(lam, max_n):
     for i in range(max_n + 1):
         probs.append([0.0] * i + p[:-i-1] + [1.0 - s])
         s -= probs[-1][-2]
-
     return np.array(probs, dtype=np.float)
 
+def reward(outer):
+    for (y,x) in np.ndindex(outer.shape):
+        outer[y][x] *= outer.shape[0] + outer.shape[1] - y - x - 2
+    return outer
+
+def rewards(rents1, rents2, car_cost):
+    r = np.empty_like(rents1)
+    for (y,x) in np.ndindex(rents1.shape):
+        outer = np.outer(rents1[y][:y + 1], rents2[x][:x + 1]) 
+        r[y][x] = np.sum(car_cost * reward(outer)) 
+    return r
+
+#def cars_rented(max_n):
+#    cars_rented = np.empty((max_n + 1, max_n + 1))
+#    for (y, x) in np.ndindex(cars_rented.shape):
+#        cars_rented[y][x] = 2 * max_n - y - x
+#    return cars_rented
 
 if __name__ == "__main__":
     np.set_printoptions(formatter={'float': '{: 0.2f}'.format}, linewidth=200)
-    print(poisson(5, 20))
 
+    rents1 = np.flipud(np.fliplr(poisson(RENTS_1, MAX_CARS)))
+    rents2 = np.flipud(np.fliplr(poisson(RENTS_2, MAX_CARS)))
+    returns1 = poisson(RETURNS_1, MAX_CARS)
+    returns2 = poisson(RETURNS_2, MAX_CARS)
+    
+    test1 = np.array([[3,0,0],[1,2,0],[0,1,2]])
+    test2 = np.array([[5,0,0],[3,2,0],[1,2,2]])
+    print(rents1)
+    print(rents2)
+    print(rewards(rents1, rents2, CAR_COST))
