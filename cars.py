b6154a2bc9aea34967a81a2379dea1137c701975
grzesiu <grzspychala@gmail.com>
2017-05-09 14:25:10 +0200

    Evaluate policy

--- cars.py
+++ cars.py
@@ -15,7 +15,7 @@ DISCOUNT_FACTOR = 0.9
 N_E = 5
 
 
-def poisson(lam, max_n):
+def get_poisson(lam, max_n):
     f = lambda n: lam ** n * math.exp(-lam) / math.factorial(n)
     p = [f(n) for n in range(max_n + 1)]
     s = sum(p[:-1])
@@ -26,21 +26,21 @@ def poisson(lam, max_n):
     return np.array(probs, dtype=np.float)
 
 
-def cars_rented(rents_no_prob):
+def get_cars_rented(rents_no_prob):
     for (y,x) in np.ndindex(rents_no_prob.shape):
         rents_no_prob[y][x] *= rents_no_prob.shape[0] + rents_no_prob.shape[1] - y - x - 2
     return rents_no_prob
 
 
-def rewards(rents1, rents2, car_cost):
+def get_rewards(rents1, rents2, car_cost):
     r = np.empty_like(rents1)
     for (y,x) in np.ndindex(rents1.shape):
         rents_no_prob = np.outer(rents1[y][:y + 1], rents2[x][:x + 1]) 
-        r[y][x] = np.sum(car_cost * cars_rented(rents_no_prob)) 
+        r[y][x] = np.sum(car_cost * get_cars_rented(rents_no_prob)) 
     return r
 
 
-def possible_actions(max_cars, max_move):
+def get_possible_actions(max_cars, max_move):
     shape = max_cars + 1, max_cars + 1
     possible_actions = np.empty(shape, dtype=object)
     for (y, x) in np.ndindex(shape):
@@ -49,25 +49,68 @@ def possible_actions(max_cars, max_move):
     return possible_actions
 
 
-def init_policy(max_cars, possible_actions):
+def get_init_policy(max_cars, possible_actions):
     policy = np.empty((max_cars + 1, max_cars + 1), dtype=np.int)
     for s in np.ndindex(policy.shape):
-        self.policy[s] = np.random.choice(possible_actions[s])
+        policy[s] = np.random.choice(possible_actions[s])
     return policy
    
 
-def probs(rents1, returns1, rents2, returns2):
-    return np.einsum('ij,kl->ijkl', rents1 @ returns1, rents2 @ returns2)
+def get_probs(rents1, returns1, rents2, returns2):
+    return np.einsum('ij,kl->ikjl', rents1 @ returns1, rents2 @ returns2)
 
-if __name__ == "__main__":
-    np.set_printoptions(formatter={'float': '{: 0.2f}'.format}, linewidth=200)
 
-    rents1 = np.flipud(np.fliplr(poisson(RENTS1, MAX_CARS)))
-    rents2 = np.flipud(np.fliplr(poisson(RENTS2, MAX_CARS)))
-    returns1 = poisson(RETURNS1, MAX_CARS)
-    returns2 = poisson(RETURNS2, MAX_CARS)
+def get_next_utility(rewards, policy, utility, probs, discount_factor, move_cost):
+    next_utility = np.empty_like(utility, dtype=np.float)
+    for (y, x) in np.ndindex(utility.shape):
+        ya = y - policy[y][x]
+        xa = x + policy[y][x]
+        r = rewards[ya][xa] - abs(policy[y][x]) * move_cost
+        next_utility[y][x] = r + discount_factor * np.sum(probs[ya][xa] * utility)
+    return next_utility
+
+
+def get_next_policy(possible_actions, probs, utility):
+    next_policy = np.empty_like(utility, dtype=np.int)
+    for (y, x) in np.ndindex(next_policy.shape):
+        for actions in possible_actions[y][x]:
+            m = 0
+            for a in np.nditer(actions):
+                ua = np.sum(probs[y - a][x + a] * utility)
+                if ua > m:
+                    m = ua
+                    next_policy[y][x] = a
+    return next_policy
+
+
+def iterate_policy(rewards, possible_actions, probs, discount_factor, max_cars, move_cost, k):
+    print(possible_actions)
+    next_policy = get_init_policy(max_cars, possible_actions)
+    policy = np.empty_like(next_policy, dtype=np.int)
+    utility = np.zeros_like(rewards, dtype=np.int)
+
+    while not np.array_equal(next_policy, policy):
+        policy = next_policy
+        for _ in range(k):
+            utility = get_next_utility(rewards, policy, utility, probs, discount_factor, move_cost)
+
+        next_policy = get_next_policy(possible_actions, probs, utility)
+        print(utility)
+        print(next_policy)
     
-    rewards = rewards(rents1, rents2, CAR_COST)
-    possible_actions = possible_actions(MAX_CARS, MAX_MOVE)
-    print(probs(rents1, returns1, rents2, returns2).shape)
+    
+if __name__ == "__main__":
+    np.set_printoptions(formatter={'float': '{: 0.2f}'.format}, linewidth=100000)
 
+    rents1 = np.flipud(np.fliplr(get_poisson(RENTS1, MAX_CARS)))
+    rents2 = np.flipud(np.fliplr(get_poisson(RENTS2, MAX_CARS)))
+    returns1 = get_poisson(RETURNS1, MAX_CARS)
+    returns2 = get_poisson(RETURNS2, MAX_CARS)
+    
+    rewards = get_rewards(rents1, rents2, CAR_COST)
+
+    possible_actions = get_possible_actions(MAX_CARS, MAX_MOVE)
+
+    probs = get_probs(rents1, returns1, rents2, returns2)
+
+    iterate_policy(rewards, possible_actions, probs, DISCOUNT_FACTOR, MAX_CARS, MOVE_COST, 1)
